c97baa92b70d9bc5cacad257ae25133890513138
Marthur25 <meggan.a85@gmail.com>
2022-06-09 20:58:49 +1000

    tidy js

--- src/index.js
+++ src/index.js
@@ -9,21 +9,6 @@ let days = [
   "Saturday"
 ];
 
-//let months = [
-//    "January",
-//    "February",
-//    "March",
-//    "April",
-//    "May",
-//    "June",
-//    "July",
-//    "August",
-//    "September",
-//    "October",
-//    "November",
-//    "December"
-//];
-
 let currentTime = new Date();
 let currentDay = days[currentTime.getDay()];
 let currentHour = currentTime.getHours();
@@ -38,6 +23,41 @@ let formatTime = `${currentDay} ${currentHour}:${currentMinutes}`;
 
 let time = document.querySelector("#get-time");
 time.innerHTML = `${formatTime}`;
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let locationElement = document.querySelector("#location");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 //2
 function showTemperature(response) {
@@ -89,6 +109,9 @@ function searchLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
+
+
+//mine
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", updateSubmit);
