75619e63358be934ead71820877273dbc961de7e
Marthur25 <meggan.a85@gmail.com>
2022-06-15 19:29:11 +1000

    added repeating html block to JS

--- src/index.js
+++ src/index.js
@@ -28,8 +28,39 @@ function formatDay(timestamp) {
   let day = date.getDay();
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 
   return days[day];
+}
+
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = 
+  ` <div class="row">`; forecast.forEach(function (forecastDay, index) { if (index < 5) { 
+    forecastHTML +=
+    ` <div class="col-2">
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png" alt="" width="42" />
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max"> ${Math.round( forecastDay.temp.max )}° </span>
+      <span class="weather-forecast-temperature-min"> ${Math.round( forecastDay.temp.min )}° </span>
+    </div>
+</div> `; 
+} 
+});
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 
@@ -71,11 +102,15 @@ function searchLocation(position) {
 }
 
 function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
 
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -84,6 +119,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
   }
   
 
